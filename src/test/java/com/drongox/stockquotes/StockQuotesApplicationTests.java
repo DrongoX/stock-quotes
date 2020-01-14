a511ef7326f9b4754bf5cb4b1a49523be6374d13
Ilya Zinchuk <ilya.zinchuk@happn.fr>
2020-01-14 12:35:28 +0100

    initial state

---
+++ src/test/java/com/drongox/stockquotes/StockQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drongox.stockquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
